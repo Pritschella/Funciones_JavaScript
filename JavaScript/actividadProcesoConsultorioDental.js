631f0c195d94d8327ab1f1f48c357dfdebb21393
Pritschella <prits99@hotmail.com>
2023-09-04 20:56:56 -0600

    Se agregó la actividad proceso del consultorio dental con funciones.

--- JavaScript/actividadProcesoConsultorioDental.js
+++ JavaScript/actividadProcesoConsultorioDental.js
@@ -1,2 +1,30 @@
+//Programa para atender paciente
+
+function verificarPaciente(nombre){
+    console.log("El/la paciente " + nombre +  " está registrad@ en las citas del día.");
+    
+}
+
+function verificarFechaPaciente(cita){
+    console.log("El paciente tiene cita el día " + cita);
+    let fechaCita = cita + " está agendada";
+    
+    console.log("Su cita del " + fechaCita + " por consultorio Feliz");
+    fechaCita;
+}
+
+function verificarHora(horaCita, minutosCita){
+    console.log("La hora de la cita es a las: " + horaCita + " horas con " + minutosCita + " minutos.");
+}
+
+function pasarPaciente(){
+    verificarPaciente("Berenice"); 
+    
+    verificarFechaPaciente("14/Septiembre/2023");
+
+    verificarHora(13, 30);
+}
+
+pasarPaciente();
 
 
